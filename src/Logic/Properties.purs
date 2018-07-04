ba4501aeb508e2c621fb55b6363282056a7a5edf
Michael A <nomicflux@gmail.com>
2018-07-04 13:22:57 -0500

    Relational Properties

--- src/Logic/Properties.purs
+++ src/Logic/Properties.purs
@@ -1,11 +1,37 @@
-module Logic.Properties where
+module Logic.Properties
+       ( Property
+       , reflexive
+       , symmetric
+       , transitive
+       ) where
 
-import Data.Foldable (class Foldable)
+import Data.Array as A
+import Data.Function (($))
+import Data.List (List)
+import Data.List as L
 import Logic.RelationMap (RelationMap)
+import Logic.RelationMap as R
+import Prelude ((-))
 
-data Property f a = Property (
-  Foldable f =>
-  f a ->
+type Property =
   RelationMap ->
   RelationMap
-  )
+
+varRange :: RelationMap -> List Int
+varRange rels = L.range 0 ((R.numVars rels) - 1)
+
+reflexive :: Property
+reflexive rels =
+  L.foldl (\acc var -> R.addRelation var var acc) rels (varRange rels)
+
+symmetric :: Property
+symmetric rels =
+  L.foldl (\acc var -> R.addRelationsTo (R.getRelationsFrom var rels) var acc) rels (varRange rels)
+
+transitive :: Property
+transitive rels =
+  L.foldl (\acc var ->
+            R.addRelations var (A.concatMap (\v ->
+                                              R.getRelationsFrom v rels)
+                                $ R.getRelationsFrom var rels)
+            acc) rels (varRange rels)
