3d8e45dc5c361c4efe016d49a8a626768520aa49
Michael A <nomicflux@gmail.com>
2018-07-03 21:27:53 -0500

    Added constants

---
+++ src/Component/Common/Constants.purs
@@ -0,0 +1,13 @@
+module Component.Common.Constants where
+
+import Data.Int (toNumber)
+import Prelude ((*))
+
+worldRadius :: Int
+worldRadius = 25
+
+selfRelationRadius :: Number
+selfRelationRadius = (toNumber worldRadius) * 0.75
+
+arrowHeadLength :: Number
+arrowHeadLength = (toNumber worldRadius) * 0.25
